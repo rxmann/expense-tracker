c7644107af1ba38d7db81a7f780fa73f19aebc1f
rxman <k.romann413@gmail.com>
2024-09-01 19:25:46 +0545

    session with hibernate config and simple save

---
+++ src/main/java/com/codex/AlienName.java
@@ -0,0 +1,44 @@
+package com.codex;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class AlienName {
+
+    public String fname;
+    public String lname;
+    public String mname;
+
+    public String getLname() {
+        return lname;
+    }
+
+    @Override
+    public String toString() {
+        return "AlienName{" +
+                "fname='" + fname + '\'' +
+                ", lname='" + lname + '\'' +
+                ", mname='" + mname + '\'' +
+                '}';
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getMname() {
+        return mname;
+    }
+
+    public void setMname(String mname) {
+        this.mname = mname;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+}
--- src/main/java/com/codex/Main.java
+++ src/main/java/com/codex/Main.java
@@ -1,18 +1,68 @@
 package com.codex;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-
-// @SpringBootApplication: Configure the app as Spring Boot managed
-// combination of 3 annotations: @EnableAutoConfiguration, @ComponentScan, @SpringBootConfiguration
-@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
 
-        System.out.println("Hello World!");
+//        AlienName an = new AlienName();
+//        an.setFname("dxman");
+//        an.setLname("bark");
+
+        Alien alien = new Alien();
+//        alien.setColor("blue");
+//        alien.setAname(an);
+
+        // Create Configuration instance
+        Configuration configuration = new Configuration().configure().addAnnotatedClass(Alien.class);
+
+        // Create StandardServiceRegistry
+        StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+
+        // Create SessionFactory
+        SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            // Open a new session
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            // Commit transaction
+            transaction.commit();
+
+            // save
+            if (alien != null) {
+                session.save(alien);
+            }
+            // get
+//            alien = session.get(Alien.class, 1);
+
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                // Rollback transaction on error
+                transaction.rollback();
+            }
+            // Handle the exception (log it, rethrow it, etc.)
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                // Ensure session is closed
+                session.close();
+            }
+            if (sessionFactory != null) {
+                // Ensure sessionFactory is closed
+                sessionFactory.close();
+            }
+        }
+
     }
 }
